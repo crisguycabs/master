2e61a9717c7a2a621f5a70022ae375dfc700fe47
crisguycabs <crisguycabs@gmail.com>
2017-12-05 14:30:54 -0500

    lista la reducción de memoria de RS, cambios menores pendientes

--- RockStatic/Forms/CurvasForm.cs
+++ RockStatic/Forms/CurvasForm.cs
@@ -33,7 +33,7 @@ namespace RockStatic
         /// contador de pertenencia de slide
         /// </summary>
         public double[] SlidePertenece;
-        
+
 
         /// <summary>
         /// Variable intermedia para el calculo de propiedades estaticas
@@ -144,19 +144,6 @@ namespace RockStatic
                     desvP1high.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
                 }
 
-                // para el phantom1 Low                
-                for (int i = 0; i < padre.actual.datacuboLow.dataCube.Count; i++)
-                {
-                    List<double> numerosCT = new List<double>();
-                    for (int j = 0; j < padre.actual.datacuboLow.dataCube[i].segPhantom1.Count; j++)
-                    {
-                        if (padre.actual.datacuboLow.dataCube[i].segPhantom1[j] > 0) numerosCT.Add((double)padre.actual.datacuboLow.dataCube[i].segPhantom1[j]);
-                    }
-
-                    meanP1low.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCT));
-                    desvP1low.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
-                }
-
                 // para el phantom2 High
 
                 for (int i = 0; i < padre.actual.datacuboHigh.dataCube.Count; i++)
@@ -169,19 +156,6 @@ namespace RockStatic
 
                     meanP2high.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCT));
                     desvP2high.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
-                }
-                // para el phantom2 Low
-
-                for (int i = 0; i < padre.actual.datacuboLow.dataCube.Count; i++)
-                {
-                    List<double> numerosCT = new List<double>();
-                    for (int j = 0; j < padre.actual.datacuboLow.dataCube[i].segPhantom2.Count; j++)
-                    {
-                        if (padre.actual.datacuboLow.dataCube[i].segPhantom2[j] > 0) numerosCT.Add((double)padre.actual.datacuboLow.dataCube[i].segPhantom2[j]);
-                    }
-
-                    meanP2low.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCT));
-                    desvP2low.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
                 }
 
                 // para el phantom3 High
@@ -197,6 +171,63 @@ namespace RockStatic
                     meanP3high.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCT));
                     desvP3high.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
                 }
+
+                // ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                padre.actual.datacuboLow = new MyDataCube();
+                padre.actual.datacuboLow.dataCube = new List<MyDicom>();
+
+                for (int i = 0; i < padre.actual.datacuboHigh.dataCube.Count; i++)
+                {
+                    // se leen uno a uno los dicom de baja para ahorrar memoria
+                    MyDicom dicom = new MyDicom(padre.actual.folderLow + "\\" + i);
+                    padre.actual.datacuboLow.dataCube.Add(new MyDicom());
+                    
+                    // segementacion del core
+                    AuxThread thread = new AuxThread(dicom.pixelData, padre.actual.areaCore.x, padre.actual.areaCore.y, padre.actual.areaCore.width, dicom.selector.Columns.Data, dicom.selector.Rows.Data, new System.Threading.ManualResetEvent(false));
+                    padre.actual.datacuboLow.dataCube[i].segCore = thread.SegmentarCircular();
+
+                    // segmentacion del Phantom1
+                    thread = new AuxThread(dicom.pixelData, padre.actual.areaPhantom1.x, padre.actual.areaPhantom1.y, padre.actual.areaPhantom1.width, dicom.selector.Columns.Data, dicom.selector.Rows.Data, new System.Threading.ManualResetEvent(false));
+                    padre.actual.datacuboLow.dataCube[i].segPhantom1 = thread.SegmentarCircular();
+
+                    // segmentacion del Phantom2
+                    thread = new AuxThread(dicom.pixelData, padre.actual.areaPhantom2.x, padre.actual.areaPhantom2.y, padre.actual.areaPhantom2.width, dicom.selector.Columns.Data, dicom.selector.Rows.Data, new System.Threading.ManualResetEvent(false));
+                    padre.actual.datacuboLow.dataCube[i].segPhantom2 = thread.SegmentarCircular();
+
+                    // segmentacion del Phantom1
+                    thread = new AuxThread(dicom.pixelData, padre.actual.areaPhantom3.x, padre.actual.areaPhantom3.y, padre.actual.areaPhantom3.width, dicom.selector.Columns.Data, dicom.selector.Rows.Data, new System.Threading.ManualResetEvent(false));
+                    padre.actual.datacuboLow.dataCube[i].segPhantom3 = thread.SegmentarCircular();
+                }
+
+
+                // para el phantom1 Low                
+                for (int i = 0; i < padre.actual.datacuboLow.dataCube.Count; i++)
+                {
+                    List<double> numerosCT = new List<double>();
+                    for (int j = 0; j < padre.actual.datacuboLow.dataCube[i].segPhantom1.Count; j++)
+                    {
+                        if (padre.actual.datacuboLow.dataCube[i].segPhantom1[j] > 0) numerosCT.Add((double)padre.actual.datacuboLow.dataCube[i].segPhantom1[j]);
+                    }
+
+                    meanP1low.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCT));
+                    desvP1low.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
+                }
+
+                // para el phantom2 Low
+
+                for (int i = 0; i < padre.actual.datacuboLow.dataCube.Count; i++)
+                {
+                    List<double> numerosCT = new List<double>();
+                    for (int j = 0; j < padre.actual.datacuboLow.dataCube[i].segPhantom2.Count; j++)
+                    {
+                        if (padre.actual.datacuboLow.dataCube[i].segPhantom2[j] > 0) numerosCT.Add((double)padre.actual.datacuboLow.dataCube[i].segPhantom2[j]);
+                    }
+
+                    meanP2low.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCT));
+                    desvP2low.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCT));
+                }
+
                 // para el phantom3 Low
 
                 for (int i = 0; i < padre.actual.datacuboLow.dataCube.Count; i++)
@@ -214,7 +245,7 @@ namespace RockStatic
             else
             {
                 // se preparan los generadores de CT aleatorios para cada phantom
-                for (int i = 0; i < padre.actual.datacuboLow.dataCube.Count; i++)
+                for (int i = 0; i < padre.actual.datacuboHigh.dataCube.Count; i++)
                 {
                     meanP1high.Add(padre.actual.phantom1.mediaHigh);
                     desvP1high.Add(padre.actual.phantom1.desvHigh);
@@ -230,7 +261,7 @@ namespace RockStatic
                     meanP3low.Add(padre.actual.phantom3.mediaLow);
                     desvP3low.Add(padre.actual.phantom3.desvLow);
                 }
-                                
+
             }
 
             // se preparan los vectores para densidad y zeff
@@ -247,7 +278,6 @@ namespace RockStatic
             int iarea;
 
             // se empieza a recorrer cada slide que se encuentre dentro de las areas de interes
-            // para cada slide se genera una colección de datos aleatorios simulando cada phantom, y se estima la media para cada phantom en cada slide
             bool slide = false;
             for (int i = 0; i < padre.actual.datacuboHigh.dataCube.Count; i++)
             {
@@ -274,7 +304,7 @@ namespace RockStatic
                     ctP2Low = meanP2low[i];
                     ctP3High = meanP3high[i];
                     ctP3Low = meanP3low[i];
-                    
+
 
 
                     // se resuelve el sistema lineal para obtener las constantes A,B,C,D,E,F
@@ -295,7 +325,7 @@ namespace RockStatic
                     DE = x2[0];
                     D = x2[1];
                     DF = x2[2];
-                    
+
                     // se empieza a recorrer cada voxel, en la segmentacion del actual i-slide, se revisa que este dentro del area de interes
 
                     Df = new List<double>();
@@ -304,41 +334,41 @@ namespace RockStatic
                     Pef = new List<double>();
 
 
-                    
-                    double tDf, tZf, tZeff, tPef, pb,dens;
+
+                    double tDf, tZf, tZeff, tPef, pb, dens;
 
                     // dado que se recorre fila a fila, entonces el indice j corresponde al eje Y y el indice k al eje X
-                        List<double> numerosCTlow = new List<double>();
-                        List<double> numerosCThigh = new List<double>();
-                        for (int k = 0; k < padre.actual.datacuboHigh.widthSegCore; k++)
-                        {
-                            // se calcula la media para ca core
-                            if (padre.actual.datacuboHigh.dataCube[i].segCore[k] > 0) numerosCThigh.Add((double)padre.actual.datacuboHigh.dataCube[i].segCore[k]);
-                            if (padre.actual.datacuboLow.dataCube[i].segCore[k] > 0) numerosCTlow.Add((double)padre.actual.datacuboLow.dataCube[i].segCore[k]);
-                            
-                        }
+                    List<double> numerosCTlow = new List<double>();
+                    List<double> numerosCThigh = new List<double>();
+                    for (int k = 0; k < padre.actual.datacuboHigh.widthSegCore; k++)
+                    {
+                        // se calcula la media para ca core
+                        if (padre.actual.datacuboHigh.dataCube[i].segCore[k] > 0) numerosCThigh.Add((double)padre.actual.datacuboHigh.dataCube[i].segCore[k]);
+                        if (padre.actual.datacuboLow.dataCube[i].segCore[k] > 0) numerosCTlow.Add((double)padre.actual.datacuboLow.dataCube[i].segCore[k]);
 
-                        meanCorehigh.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCThigh));
-                        desvCorehigh.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCThigh));
+                    }
 
-                        meanCorelow.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCTlow));
-                        desvCorelow.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCTlow));
+                    meanCorehigh.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCThigh));
+                    desvCorehigh.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCThigh));
 
-                        tDf = A * meanCorelow[jkindex]  - B * meanCorehigh[jkindex] + C;
-                                //Df.Add(tDf);
+                    meanCorelow.Add(MathNet.Numerics.Statistics.Statistics.Mean(numerosCTlow));
+                    desvCorelow.Add(MathNet.Numerics.Statistics.Statistics.StandardDeviation(numerosCTlow));
 
-                                pb=(1.0704 * tDf - 0.1883);
-                                      //dens = 0.9342 * pb + 0.1759;
-                                tZf = Math.Pow(((DE * meanCorelow[jkindex] - D * meanCorehigh[jkindex] - DF)/ tDf),1/3.6);
-                                //Zf.Add(tZf);
-                                //tZeff = Math.Pow(Math.Pow((tZf / (0.9342 * tDf + 0.1759)), 10), 1 / 36);
-                               // tZeff = Math.Pow((tZf / dens), 1/3.6)*10;
-                                //Zeff.Add(tZeff);
+                    tDf = A * meanCorelow[jkindex] - B * meanCorehigh[jkindex] + C;
+                    //Df.Add(tDf);
 
-                                tPef = Math.Pow((tZf / 10), 3.6);
-                                //Pef.Add(tPef);
-                              
-                        
+                    pb = (1.0704 * tDf - 0.1883);
+                    //dens = 0.9342 * pb + 0.1759;
+                    tZf = Math.Pow(((DE * meanCorelow[jkindex] - D * meanCorehigh[jkindex] - DF) / tDf), 1 / 3.6);
+                    //Zf.Add(tZf);
+                    //tZeff = Math.Pow(Math.Pow((tZf / (0.9342 * tDf + 0.1759)), 10), 1 / 36);
+                    // tZeff = Math.Pow((tZf / dens), 1/3.6)*10;
+                    //Zeff.Add(tZeff);
+
+                    tPef = Math.Pow((tZf / 10), 3.6);
+                    //Pef.Add(tPef);
+
+
                     Dfm.Add(pb);
                     Zfme.Add(tZf);
 
@@ -350,19 +380,19 @@ namespace RockStatic
                     {
                         Pefm.Add(tPef);
                     }
-                    
+
                     jkindex++;
                     SlidePertenece[i] = 1;
 
-                    }
-                    // stats = new MathNet.Numerics.Statistics.DescriptiveStatistics(Df);
-                    // Dfm[i] = stats.Mean;
+                }
+                // stats = new MathNet.Numerics.Statistics.DescriptiveStatistics(Df);
+                // Dfm[i] = stats.Mean;
 
                     // stats = new MathNet.Numerics.Statistics.DescriptiveStatistics(Zeff);
-                    // Zfme[i] = stats.Mean;
-                    //stats = new MathNet.Numerics.Statistics.DescriptiveStatistics(Pef);
-                    // Pefm[i] = stats.Mean;
-                
+                // Zfme[i] = stats.Mean;
+                //stats = new MathNet.Numerics.Statistics.DescriptiveStatistics(Pef);
+                // Pefm[i] = stats.Mean;
+
                 else
                 {
                     // se llenan los vectores de densidad y zeff con valores -1... si el valor es -1 entonces no se grafica
@@ -381,13 +411,13 @@ namespace RockStatic
             profundidad = new double[padre.actual.datacuboHigh.dataCube.Count];
             double paso = Convert.ToDouble(padre.actual.tail - padre.actual.head) / Convert.ToDouble(profundidad.Length - 1);
 
-            for (int i = 0; i < profundidad.Length;i++) profundidad[i]=padre.actual.head+(i*paso);
-            
+            for (int i = 0; i < profundidad.Length; i++) profundidad[i] = padre.actual.head + (i * paso);
+
             // se preparan los nombres de los ejes
-            chart1.ChartAreas[0].AxisY.Title="PROFUNDIDAD (" + padre.actual.unidadProfundidad + ")";
-            chart2.ChartAreas[0].AxisY.Title="PROFUNDIDAD (" + padre.actual.unidadProfundidad + ")";
+            chart1.ChartAreas[0].AxisY.Title = "PROFUNDIDAD (" + padre.actual.unidadProfundidad + ")";
+            chart2.ChartAreas[0].AxisY.Title = "PROFUNDIDAD (" + padre.actual.unidadProfundidad + ")";
             chart3.ChartAreas[0].AxisY.Title = "PROFUNDIDAD (" + padre.actual.unidadProfundidad + ")";
-            
+
             chart1.ChartAreas[0].AxisX.LabelStyle.Format = "#.##";
             chart2.ChartAreas[0].AxisX.LabelStyle.Format = "#.##";
             // chart3.ChartAreas[0].AxisX.LabelStyle.Format = "#.#######";
@@ -399,34 +429,36 @@ namespace RockStatic
             List<double> P = new List<double>();
 
 
-            PEF = new List<double>();            
+            PEF = new List<double>();
             // se cargan los puntos en el chart
             for (int i = 0; i < Dfm.Count; i++)
             {
-                
+
                 if (Dfm[i] > -1)
                 {
                     chart1.Series[0].Points.AddXY(Dfm[i], profundidad[i]);
                     D.Add(Dfm[i]);
                 }
             }
-            for(int i=0;i<Zfme.Count;i++)
+            for (int i = 0; i < Zfme.Count; i++)
             {
                 if (Zfme[i] > -1)
                 {
                     chart2.Series[0].Points.AddXY(Zfme[i], profundidad[i]);
                     Z.Add(Zfme[i]);
 
-                    if((Math.Pow(Zfme[i] / Convert.ToDouble(10), 3.6)>20))
+                    if ((Math.Pow(Zfme[i] / Convert.ToDouble(10), 3.6) > 20))
                     {
                         chart3.Series[0].Points.AddXY(-1, profundidad[i]);
                         P.Add(-1);
-                    }else{
+                    }
+                    else
+                    {
                         chart3.Series[0].Points.AddXY((Math.Pow(Zfme[i] / Convert.ToDouble(10), 3.6)), profundidad[i]);
                         P.Add((Math.Pow(Zfme[i] / Convert.ToDouble(10), 3.6)));
-                    } 
+                    }
                 }
-                
+
             }
 
             chart2.Series[0].Color = Color.Red;
@@ -437,10 +469,10 @@ namespace RockStatic
             if (P.Count < 1) P.Add(0);
 
             // se modifican los intervalos del eje
-            chart1.ChartAreas[0].AxisX.Minimum = chart1.ChartAreas[0].AxisX2.Minimum=D.Min()*0.85;
-            chart1.ChartAreas[0].AxisX.Maximum = chart1.ChartAreas[0].AxisX2.Maximum=D.Max()*1.05;
+            chart1.ChartAreas[0].AxisX.Minimum = chart1.ChartAreas[0].AxisX2.Minimum = D.Min() * 0.85;
+            chart1.ChartAreas[0].AxisX.Maximum = chart1.ChartAreas[0].AxisX2.Maximum = D.Max() * 1.05;
             chart2.ChartAreas[0].AxisX.Minimum = chart2.ChartAreas[0].AxisX2.Minimum = Z.Min() * 0.85;
-            chart2.ChartAreas[0].AxisX.Maximum = chart2.ChartAreas[0].AxisX2.Maximum=Z.Max()*1.05;
+            chart2.ChartAreas[0].AxisX.Maximum = chart2.ChartAreas[0].AxisX2.Maximum = Z.Max() * 1.05;
 
             chart3.ChartAreas[0].AxisX.Minimum = chart3.ChartAreas[0].AxisX2.Minimum = P.Min() * 0.85;
             chart3.ChartAreas[0].AxisX.Maximum = chart3.ChartAreas[0].AxisX2.Maximum = P.Max() * 1.05;
@@ -452,14 +484,14 @@ namespace RockStatic
             chart3.ChartAreas[0].AxisX.Interval = chart3.ChartAreas[0].AxisX2.Interval = (chart3.ChartAreas[0].AxisX.Maximum - chart3.ChartAreas[0].AxisX.Minimum) / 4;
 
             chart1.ChartAreas[0].AxisY.Minimum = profundidad[0];
-            chart1.ChartAreas[0].AxisY.Maximum = profundidad[profundidad.Length-1];
+            chart1.ChartAreas[0].AxisY.Maximum = profundidad[profundidad.Length - 1];
             chart2.ChartAreas[0].AxisY.Minimum = profundidad[0];
             chart2.ChartAreas[0].AxisY.Maximum = profundidad[profundidad.Length - 1];
 
             chart3.ChartAreas[0].AxisY.Minimum = profundidad[0];
             chart3.ChartAreas[0].AxisY.Maximum = profundidad[profundidad.Length - 1];
 
-        }        
+        }
 
         private void btnExportar_Click(object sender, EventArgs e)
         {
